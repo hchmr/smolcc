41eae95cdfc32c7585265de8d8fb2d2a03b8246c
hchmr <73483158+hchmr@users.noreply.github.com>
2026-06-06 14:09:52 +0200

    Remove tests for overflow in integer constants

--- tests/bad/expr/int-literal-overflow.c
+++
@@ -1,5 +0,0 @@
-//# mode: compile-only
-//# exit: 1
-//# stderr: tests/bad/expr/int-literal-overflow.c:5:13: error: integer literal overflow
-
-int value = 2147483648;
--- tests/bad/expr/uint-literal-overflow.c
+++
@@ -1,5 +0,0 @@
-//# mode: compile-only
-//# exit: 1
-//# stderr: tests/bad/expr/uint-literal-overflow.c:5:22: error: integer literal overflow
-
-unsigned int value = 4294967296U;
